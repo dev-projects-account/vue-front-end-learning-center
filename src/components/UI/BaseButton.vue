a05863aad37944d6798f333e6bc18f9fe751f096
projects <onetimeuser38@gmail.com>
2021-01-01 12:45:20 -0500

    first commit

---
+++ src/components/UI/BaseButton.vue
@@ -0,0 +1,37 @@
+<template>
+  <button :type="type" :class="mode"><slot></slot></button>
+</template>
+
+<script>
+export default {
+  props: ['type', 'mode']
+};
+</script>
+
+<style scoped>
+button {
+  padding: 0.75rem 1.5rem;
+  font-family: inherit;
+  background-color: #42b883;
+  border: 1px solid #3a0061;
+  color: white;
+  cursor: pointer;
+}
+
+button:hover,
+button:active {
+  background-color: #42b883;
+  border-color: #270041;
+}
+
+.flat {
+  background-color: transparent;
+  background-color: #42b883;
+  border: none;
+}
+
+.flat:hover,
+.flat:active {
+  background-color: #edd2ff;
+}
+</style>
---
+++ src/components/learning-resources/StoredResources.vue
@@ -0,0 +1,34 @@
+<template>
+  <ul>
+    <li>
+      <learning-resource
+        v-for="resource in resources"
+        :key="resource.id"
+        :title="resource.title"
+        :id="resource.id"
+        :description="resource.description"
+        :link="resource.link"
+      ></learning-resource>
+    </li>
+  </ul>
+</template>
+
+<script>
+import LearningResource from './LearningResource.vue';
+export default {
+  inject: ['resources'],
+  components: {
+    LearningResource
+  }
+};
+</script>
+
+<style>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  margin: auto;
+  max-width: 40rem;
+}
+</style>
